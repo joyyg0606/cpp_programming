a5939a5db905e36f6cda81e6d7e06d7d7b1890f2
vxntcy <joyyg0606@gmail.com>
2024-06-24 16:45:44 -0400

    testing for equality

--- Section 8 Statements and Operators/section_8_practice.cpp
+++ Section 8 Statements and Operators/section_8_practice.cpp
@@ -71,8 +71,8 @@ int main() {
 
 // increment and decrement operators
 
-    int counter {10};
-    int result {0};
+    // int counter {10};
+    // int result {0};
 
 // example 1 - simple increment
 
@@ -123,15 +123,70 @@ int main() {
 
 // example 5
 
-    counter = 10;
-    result = 0;
+    // counter = 10;
+    // result = 0;
 
-    cout << "Counter: " << counter << endl;
+    // cout << "Counter: " << counter << endl;
 
-    result = counter++ + 10;
+    // result = counter++ + 10;
     
-    cout << "Counter: " << counter << endl;
-    cout << "Result: " << result << endl;
+    // cout << "Counter: " << counter << endl;
+    // cout << "Result: " << result << endl;
+
+    // int total {};
+    // int num1 {}, num2 {}, num3 {};
+    // const int count{3};
+
+    // cout << "Enter 3 integers separated by spaces: ";
+    // cin >> num1 >> num2 >> num3;
+
+    // total = num1 + num2 + num3;
+
+    // double average {0.0};
+    // average = static_cast<double>(total) / count;
+    // average = (double)total/count; // old style
+
+    // cout << "The 3 numbers were: " << num1 << ", " << num2 << ", " << num3 << endl;
+    // cout << "The sum of the number is: " << total << endl;
+    // cout << "The average of the number is: " << average << endl;
+
+// testing for equality
+
+    bool equal_result {false};
+    bool not_equal_result {false};
+
+    int num1{}, num2{};
+    cout << boolalpha; //will display true/false instead of 1/0
+
+    // cout << "Enter two integers separated by a space: ";
+    // cin >> num1 >> num2;
+    // equal_result = (num1 == num2);
+    // not_equal_result = (num1 != num2);
+    // cout << "Comparison result (equals): " << equal_result << endl;
+    // cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    // char char1{}, char2{};
+    // cout << "Enter two characters separated by a space: ";
+    // cin >> char1 >> char2;
+    // equal_result = (char1 == char2);
+    // not_equal_result = (char1 != char2);
+    // cout << "Comparison result (equals): " << equal_result <<endl;
+    // cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    double double1{}, double2{};
+    // cout << "Enter two doubles separated by a space: ";
+    // cin >> double1 >> double2;
+    // equal_result = (double1 == double2);
+    // not_equal_result = (double1 != double2);
+    // cout << "Comparison result (equals): " << equal_result <<endl;
+    // cout << "Comparison result (not equals): " << not_equal_result << endl;
+
+    cout << "Enter an integer and a double separated by a space: ";
+    cin >> num1 >> double1;
+    equal_result = (num1 == double1);
+    not_equal_result = (num1 != double1);
+    cout << "Comparison result (equals): " << equal_result << endl;
+    cout << "Comparison result (not equals): " << not_equal_result << endl;
 
 
     return 0;
