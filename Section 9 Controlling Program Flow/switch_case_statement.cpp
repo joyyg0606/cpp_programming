45b336f39f2c32e5483d1843064d2d8907e6f9f4
vxntcy <joyyg0606@gmail.com>
2024-06-25 15:45:00 -0400

    switch with enumeration

--- Section 9 Controlling Program Flow/switch_case_statement.cpp
+++ Section 9 Controlling Program Flow/switch_case_statement.cpp
@@ -4,45 +4,67 @@ using namespace std;
 
 int main()
 {
-    char letter_grade {};
+// switch practice
 
-    cout << "Enter the letter grade you want for the exam: ";
-    cin >> letter_grade;
+    // char letter_grade {};
 
-    switch (letter_grade) {
-        case 'a':
-        case 'A':
-            cout << "You need a 90 or above" << endl;
+    // cout << "Enter the letter grade you want for the exam: ";
+    // cin >> letter_grade;
+
+    // switch (letter_grade) {
+    //     case 'a':
+    //     case 'A':
+    //         cout << "You need a 90 or above" << endl;
+    //         break;
+    //     case 'b':
+    //     case 'B':
+    //         cout << "You need a 80-89" << endl;
+    //         break;
+    //     case 'c':
+    //     case 'C':
+    //         cout << "You need a 70-79" << endl;
+    //         break;
+    //     case 'd':
+    //     case 'D':
+    //         cout << "You should strive for a better grade, 60-69" << endl;
+    //         break;
+    //     case 'f':
+    //     case 'F':
+    //     {
+    //         char confirm {};
+    //         cout << "Are you sure (Y/N)? ";
+    //         cin >> confirm;
+    //         if (confirm == 'y' || confirm == 'Y') {
+    //             cout << "Failure." << endl;
+    //         } else if (confirm == 'n' || confirm == 'N') {
+    //             cout << "That's what I thought";
+    //         } else {
+    //             cout << "Invalid Choice" << endl;
+    //         }
+    //         break;
+    //     }
+    //     default:
+    //         cout << "Invalid grade" << endl;
+    // }
+
+
+// switch with enumeration
+
+    enum Direction {
+        left, right, up, down
+    };
+
+    Direction heading {left};
+
+    switch (heading) {
+        case left:
+            cout << "Going left" << endl;
             break;
-        case 'b':
-        case 'B':
-            cout << "You need a 80-89" << endl;
+        case right:
+            cout << "Going left" << endl;
             break;
-        case 'c':
-        case 'C':
-            cout << "You need a 70-79" << endl;
-            break;
-        case 'd':
-        case 'D':
-            cout << "You should strive for a better grade, 60-69" << endl;
-            break;
-        case 'f':
-        case 'F':
-        {
-            char confirm {};
-            cout << "Are you sure (Y/N)? ";
-            cin >> confirm;
-            if (confirm == 'y' || confirm == 'Y') {
-                cout << "Failure." << endl;
-            } else if (confirm == 'n' || confirm == 'N') {
-                cout << "That's what I thought";
-            } else {
-                cout << "Invalid Choice" << endl;
-            }
-            break;
-        }
         default:
-            cout << "Invalid grade" << endl;
+            cout << "ok" << endl;
     }
 
     return 0;
