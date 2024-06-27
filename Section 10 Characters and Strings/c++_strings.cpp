5f1da91430f6c7296570357a0705d4f804add653
vxntcy <joyyg0606@gmail.com>
2024-06-27 15:59:48 -0400

    strings c++

--- Section 10 Characters and Strings/c++_strings.cpp
+++ Section 10 Characters and Strings/c++_strings.cpp
@@ -34,10 +34,49 @@ int main ()
     s3 = s5 + " and " + s2 + " juice"; // apple and banana juice
     cout << s3 << endl;
 
+    // s3 = "nice" + "cold" + s5 + "juice"; //compiler error
+
+    s1 = "Apple";
+    for (size_t i {0}; i < s1.length(); ++i) {
+        cout << s1.at(i);
+    }
+
+    // range base for loop
+    for (char c : s1) {
+        cout << c;
+    }
+
+    s1 = "This is a test";
+
+    cout << s1.substr(0,4) << endl; // This
+
+    // Erase
+
+    s1.erase(0,5); // "is a test"
+    cout << "s1 is now: " << s1 << endl;
+
+    // getline
     
+    string full_name {};
+    cout << "Enter  your full name: ";
+    getline(cin, full_name);
+    
+    cout << "You full name is: " << full_name << endl;
 
 
+    // Find
+    s1 = "The secret word is Boo";
+    string word {};
 
+    cout << "Enter the word to find: ";
+    cin >> word;
 
+    size_t position = s1.find(word);
+    if (position != string::npos) {
+        cout << "Found " << word << " at position: " << position << endl;
+    } else {
+        cout << "Sorry, " << word << " not found" << endl;
+    }
+    
     return 0;
 }
