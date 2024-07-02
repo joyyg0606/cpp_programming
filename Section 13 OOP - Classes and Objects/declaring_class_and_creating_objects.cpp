9ec1362750d1b9ba5f86448e2c9bdf48c1329cf9
vxntcy <joyyg0606@gmail.com>
2024-07-02 18:46:38 -0400

    accessing class members

--- Section 13 OOP - Classes and Objects/declaring_class_and_creating_objects.cpp
+++ Section 13 OOP - Classes and Objects/declaring_class_and_creating_objects.cpp
@@ -5,43 +5,49 @@
 using namespace std;
 
 class Player {
+public:
     // attributes
-    string name {"Player"};
-    int health {0};
-    int xp {3};
+    string name;
+    int health;
+    int xp;
 
     // methods
-    void talk(string);
+    void talk(string text_to_say) {cout << name << " says " << text_to_say << endl;};
     bool is_dead();
 };
 
 class Account {
+public:
     // attributes
-    string name {"Account"};
-    double balance {0.0};
+    string name;
+    double balance;
 
     // methods
-    bool deposit(double);
-    bool withdraw(double);
+    bool deposit(double bal){balance += bal; cout << "In deposit" << endl;}
+    bool withdraw(double bal){balance -= bal; cout << "In withdraw" << endl;}
 
 };
 
 int main(){
     Account frank_account;
-    Account jim_account;
+    frank_account.name = "Frank's account";
+    frank_account.balance = 5000.0;
+
+    frank_account.deposit(1000.0);
+    frank_account.withdraw(500.0);
 
     Player frank;
-    Player hero;
+    frank.name = "frank";
+    frank.health = 100;
+    frank.xp = 12;
+    frank.talk("Hi there");
 
-    Player players[]{frank, hero};
+    Player *enemy = new Player;
+    (*enemy).name = "Enemy";
+    (*enemy).health = 100;
+    enemy->xp = 15;
 
-    vector<Player> player_vec {frank};
-    player_vec.push_back(hero);
-
-    Player *enemy {nullptr};
-    enemy = new Player;
-
-    delete enemy;
+    enemy->talk("I will destroy you");
 
     return 0;
 }
