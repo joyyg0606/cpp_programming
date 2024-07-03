e2a06cf4468be65fe4e27fb942712f880542cb90
vxntcy <joyyg0606@gmail.com>
2024-07-03 18:10:02 -0400

    more efficient constructor initialization list

--- Section 13 OOP - Classes and Objects/constructor_initialization_lists.cpp
+++ Section 13 OOP - Classes and Objects/constructor_initialization_lists.cpp
@@ -15,13 +15,18 @@ public:
     Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() : name{"None"}, health{0}, xp{0} {
+Player::Player() : Player{"None",0,0} {
+    cout << "No-args constructor" << endl;
 }
 
-Player::Player(std::string name_val) : name{name_val}, health{0}, xp{0} {
+// Player::Player(std::string name_val) : name{name_val}, health{0}, xp{0} {
+// }
+Player::Player(std::string name_val) : Player{name_val,0,0} { // more efficient method
+    cout << "One-args constructor" << endl;
 }
 
 Player::Player(std::string name_val, int health_val, int xp_val) : name{name_val}, health{health_val}, xp{xp_val} {
+    cout << "Three args- constructor" << endl;
 }
 
 int main() {
