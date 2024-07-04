6274309f4adf4e815acbcc1b667efcd786761ebe
vxntcy <joyyg0606@gmail.com>
2024-07-03 20:43:53 -0400

    constructor parameters and default values

--- Section 13 OOP - Classes and Objects/constructor_initialization_lists.cpp
+++ Section 13 OOP - Classes and Objects/constructor_initialization_lists.cpp
@@ -10,28 +10,27 @@ private:
 
 public: 
     // overloaded constructors
-    Player();
-    Player(std::string name_val);
-    Player(std::string name_val, int health_val, int xp_val);
+    Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
 };
 
-Player::Player() : Player{"None",0,0} {
-    cout << "No-args constructor" << endl;
-}
+// Player::Player() : Player{"None",0,0} {
+//     cout << "No-args constructor" << endl;
+// }
 
 // Player::Player(std::string name_val) : name{name_val}, health{0}, xp{0} { // constructor initialization list
 // }
-Player::Player(std::string name_val) : Player{name_val,0,0} { // delegating constructors
-    cout << "One-args constructor" << endl;
-}
+// Player::Player(std::string name_val) : Player{name_val,0,0} { // delegating constructors
+//     cout << "One-args constructor" << endl;
+// }
 
 Player::Player(std::string name_val, int health_val, int xp_val) : name{name_val}, health{health_val}, xp{xp_val} {
-    cout << "Three args- constructor" << endl;
+    cout << "Three args - constructor" << endl;
 }
 
 int main() {
     Player empty;
     Player frank {"Frank"};
+    Player her {"hero", 100};
     Player villain {"Villain", 100, 55};
 
     return 0;
