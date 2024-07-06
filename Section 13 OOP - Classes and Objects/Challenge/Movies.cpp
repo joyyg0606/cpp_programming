787440dab7c66425823c367a6813cd2c51dde69e
vxntcy <joyyg0606@gmail.com>
2024-07-06 17:33:59 -0400

    challenge section 13 complete

--- Section 13 OOP - Classes and Objects/Challenge/Movies.cpp
+++ Section 13 OOP - Classes and Objects/Challenge/Movies.cpp
@@ -4,21 +4,42 @@
 #include <string>
 #include "Movie.h"
 
-class Movies
-{
-private:
-    std::vector<Movie> movies;
-public:
-    Movies();
-    ~Movies();
+Movies::Movies() {
 
-    bool Movies::add_movie(std::string name, std::string rating, int watched) {
+}
+
+Movies::~Movies() {
+
+}
+
+bool Movies::add_movie(std::string name, std::string rating, int watched) {
+    for (const Movie &movie: movies) {
+        if (movie.get_name() == name) {
+            return false;
+        }
     }
+    Movie temp {name, rating, watched};
+    movies.pushback(temp);
+    return true;
+}
 
-    bool Movies::increment_watched(std::string name) {
+bool Movies::increment_watched(std::string name) {
+    for(Movie &movie: movies) {
+        if (movie.get_name() == name) {
+            movie.increment_watched();
+            return true;
+        }
     }
+    return false;
+}
 
-    void display() const;
-};
-
-#endif
+void Movies::display() const {
+    if (movies.size() == 0) {
+        std:;cout << "No movies to display\n" << std::endl;
+    } else {
+        std::cout << "\n=====================" << std::endl;
+        for (const auto &movie: movies) {
+            movie.display();
+        }
+    }
+}
