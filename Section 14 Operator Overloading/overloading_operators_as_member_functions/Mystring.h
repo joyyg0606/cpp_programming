443bde234f6a00ea66bde41280a17e50e6ee22d0
vxntcy <joyyg0606@gmail.com>
2024-07-07 19:12:59 -0400

    overloading operators as member functions

---
+++ Section 14 Operator Overloading/overloading_operators_as_member_functions/Mystring.h
@@ -0,0 +1,26 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring {
+private:
+    char *str; //pointer to a char[] that holds C-style string
+public:
+    Mystring();
+    Mystring(const char *s);
+    Mystring(const Mystring &source); //copy consturctor
+    Mystring(Mystring &&source); //move constructor
+    ~Mystring();
+    
+    Mystring &operator = (const Mystring &rhs); //copy assignment
+    Mystring &operator = (Mystring &&rhs); //move assignment
+
+    Mystring operator-() const; //make lowercase
+    Mystring operator+(const Mystring &rhs) const; //concatenate
+    bool operator==(const Mystring &rhs) const;
+
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+};
+
+#endif
