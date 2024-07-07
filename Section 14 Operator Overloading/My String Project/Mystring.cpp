b6f617eec1b9fe707ae9347c7eb0b4a57bf8254c
vxntcy <joyyg0606@gmail.com>
2024-07-06 23:31:50 -0400

    mystring.cpp done

--- Section 14 Operator Overloading/My String Project/Mystring.cpp
+++ Section 14 Operator Overloading/My String Project/Mystring.cpp
@@ -4,7 +4,7 @@
 
 //no args constructor
 Mystring::Mystring() : str{nullptr} {
-    std = new char[1];
+    str = new char[1];
     *str = '\0';
 }
 
@@ -24,3 +24,19 @@ Mystring::Mystring(const Mystring &source) :str{nullptr} {
     str = new char[std::strlen(source.str) + 1];
     std::strcpy(str,source.str);
 }
+
+//destructor
+Mystring::~Mystring() {
+    delete [] str;
+}
+
+//display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+//length getter
+int Mystring::get_length() const {return std::strlen(str);}
+
+//string getter
+const char *Mystring::get_str() const {return str;}
