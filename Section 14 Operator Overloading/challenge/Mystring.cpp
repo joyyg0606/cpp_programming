289de4c19ffe49f54cb98bf8397ce11926c9d446
vxntcy <joyyg0606@gmail.com>
2024-07-08 17:24:04 -0400

    section 14 challenge

---
+++ Section 14 Operator Overloading/challenge/Mystring.cpp
@@ -0,0 +1,164 @@
+#include <cstring>
+#include <iostream>
+#include "Mystring.h"
+
+// No-args constructor
+Mystring::Mystring() : str{nullptr} {
+    str = new char[1];
+    *str = '\0';
+}
+
+// Overloaded constructor
+Mystring::Mystring(const char *s) : str{nullptr} {
+    if (s == nullptr) {
+        str = new char[1];
+        *str = '\0';
+    } else {
+        str = new char[std::strlen(s) + 1];
+        std::strcpy(str, s);
+    }
+}
+
+// Copy constructor
+Mystring::Mystring(const Mystring &source) : str{nullptr} {
+    str = new char[std::strlen(source.str) + 1];
+    std::strcpy(str, source.str);
+}
+
+// Move constructor
+Mystring::Mystring(Mystring &&source) : str(source.str) {
+    source.str = nullptr;
+    std::cout << "Move constructor used" << std::endl;
+}
+
+// Destructor
+Mystring::~Mystring() {
+    delete[] str;
+}
+
+// Copy assignment
+Mystring &Mystring::operator=(const Mystring &rhs) {
+    std::cout << "Using copy assignment" << std::endl;
+    if (this == &rhs)
+        return *this;
+    delete[] str;
+    str = new char[std::strlen(rhs.str) + 1];
+    std::strcpy(str, rhs.str);
+    return *this;
+}
+
+// Move assignment
+Mystring &Mystring::operator=(Mystring &&rhs) {
+    std::cout << "Using move assignment" << std::endl;
+    if (this == &rhs)
+        return *this;
+    delete[] str;
+    str = rhs.str;
+    rhs.str = nullptr;
+    return *this;
+}
+
+// Equality
+bool Mystring::operator==(const Mystring &rhs) const {
+    return (std::strcmp(str, rhs.str) == 0);
+}
+
+// Not equals
+bool Mystring::operator!=(const Mystring &rhs) const {
+    return !(std::strcmp(str, rhs.str) == 0);
+}
+
+// Less than
+bool Mystring::operator<(const Mystring &rhs) const {
+    return (std::strcmp(str, rhs.str) < 0);
+}
+
+// Greater than
+bool Mystring::operator>(const Mystring &rhs) const {
+    return (std::strcmp(str, rhs.str) > 0);
+}
+
+// Make lowercase
+Mystring Mystring::operator-() const {
+    char *buff = new char[std::strlen(str) + 1];
+    std::strcpy(buff, str);
+    for (size_t i = 0; i < std::strlen(buff); i++)
+        buff[i] = std::tolower(buff[i]);
+    Mystring temp{buff};
+    delete[] buff;
+    return temp;
+}
+
+// Concatenate
+Mystring Mystring::operator+(const Mystring &rhs) const {
+    char *buff = new char[std::strlen(str) + std::strlen(rhs.str) + 1];
+    std::strcpy(buff, str);
+    std::strcat(buff, rhs.str);
+    Mystring temp{buff};
+    delete[] buff;
+    return temp;
+}
+
+// Concatenate and assign
+Mystring &Mystring::operator+=(const Mystring &rhs) {
+    *this = *this + rhs;
+    return *this;
+}
+
+// Repeat
+Mystring Mystring::operator*(int n) const {
+    Mystring temp;
+    for (int i = 1; i <= n; i++)
+        temp = temp + *this;
+    return temp;
+}
+
+// Repeat and assign
+Mystring &Mystring::operator*=(int n) {
+    *this = *this * n;
+    return *this;
+}
+
+// Make uppercase (prefix)
+Mystring &Mystring::operator++() {
+    for (size_t i = 0; i < std::strlen(str); i++)
+        str[i] = std::toupper(str[i]);
+    return *this;
+}
+
+// Make uppercase (postfix)
+Mystring Mystring::operator++(int) {
+    Mystring temp(*this);
+    operator++();
+    return temp;
+}
+
+// Display method
+void Mystring::display() const {
+    std::cout << str << " : " << get_length() << std::endl;
+}
+
+// Get length
+int Mystring::get_length() const {
+    return std::strlen(str);
+}
+
+// Get string
+const char *Mystring::get_str() const {
+    return str;
+}
+
+// Overloaded insertion operator
+std::ostream &operator<<(std::ostream &os, const Mystring &rhs) {
+    os << rhs.str;
+    return os;
+}
+
+// Overloaded extraction operator
+std::istream &operator>>(std::istream &in, Mystring &rhs) {
+    char *buff = new char[1000];
+    in >> buff;
+    rhs = Mystring{buff};
+    delete[] buff;
+    return in;
+}
---
+++ Section 14 Operator Overloading/challenge/Mystring.h
@@ -0,0 +1,39 @@
+#ifndef _MYSTRING_H_
+#define _MYSTRING_H_
+
+class Mystring
+{
+    friend std::ostream &operator<<(std::ostream &os, const Mystring &rhs);
+    friend std::istream &operator >> (std::istream &in, Mystring &rhs);
+private:
+    char *str; //pointe rto a char[]
+
+public:
+    Mystring();
+    Mystring(const char *s);
+    Mystring (const Mystring &source);
+    Mystring(Mystring &&source);
+    ~Mystring();
+
+    Mystring &operator=(const Mystring &rhs); //copy assignment
+    Mystring &operator=(Mystring &&rhs);
+
+    void display() const;
+    int get_length() const;
+    const char *get_str() const;
+
+    //overloaded operator member methods
+    Mystring operator -() const;
+    Mystring operator+(const Mystring &rhs) const;
+    bool operator==(const Mystring &rhs) const;
+    bool operator!=(const Mystring &rhs) const;
+    bool operator<(const Mystring &rhs) const;
+    bool operator>(const Mystring &rhs) const;
+    Mystring &operator+=(const Mystring &rhs);
+    Mystring operator *(int n) const;
+    Mystring &operator*=(int n);
+    Mystring &operator++();
+    Mystring operator++(int);
+};
+
+#endif
