af6ce6c8e4097d791d82752f8b916fae94e7febc
vxntcy <joyyg0606@gmail.com>
2024-07-10 20:28:05 -0400

    Trust_account and Account_util for challenge project

---
+++ Section 15 Inheritance/Challenge/Account_Util.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "Account_Util.h"
+
+void display(const std::vector <Account> &accounts) {
+    std::cout << "Accounts" << std::endl;
+    for (const auto &acc:accounts) 
+        std::cout << acc << std::endl;
+}
+
+void deposit(std::vector<Account> &accounts, double amount) {
+    std::cout << "Depositing to Accounts" << std::endl;
+    for (auto &acc:accounts) {
+        if (acc.deposit*(amount)) {
+            std::cout << "Deposited " << amount << " to " << acc << std::endl;
+        } else {
+            std::ciout << "failed Deposit of " << amount " to " << acc << std::endl;
+        }
+    }
+}
+
+void withdraw(std::vector<Account> &accounts, double amount) {
+    std::cout << "Withdrawing from ACcounts" << std::endl;
+    for (auto &acc:accounts) {
+        if (acc.withdraw(amount)) {
+            std::cout << "Withdrew " << amount " from " << acc << std::endl;
+        } else {
+            std::cout << "Failed Withdrawal of " << amount << " from " << acc << std::endl;
+        }
+    }
+}
+
+void display(const std::vector<Trust_Account> &accounts) {
+    std::cout << "Trust Accounts" << std::endl;
+    for (const auot &acc:accounts) {
+        std::cout << acc << std::endl;
+    }
+}
+
+void deposit(std::vector<Trust_Account> &accounts, double amount) {
+    std::cout << "Depositing to Trust Accounts" << std::endl;
+    for (auto &acc:accounts) {
+        if (acc.deposit(amount)) {
+            std::cout << "Deposited " << amount << " to " << acc << std::endl;
+        } else {
+            std::cout << "Failed Deposited of " << amount << " to " << std::endl;
+        }
+    }
+}
