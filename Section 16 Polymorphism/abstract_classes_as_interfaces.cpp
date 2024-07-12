57ee277df7ce393626e0990cef68cd02969e10b1
vxntcy <joyyg0606@gmail.com>
2024-07-12 16:11:18 -0400

    added an I_Printable and dog class

--- Section 16 Polymorphism/abstract_classes_as_interfaces.cpp
+++ Section 16 Polymorphism/abstract_classes_as_interfaces.cpp
@@ -1,59 +1,73 @@
 #include <iostream>
 
-class Account {
-    friend std::ostream &operator<<(std::ostream &os, const Account &acc);
+class I_Printable {
+    friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
+public:
+    virtual void print(std::ostream &os) const = 0;
+    virtual ~I_Printable() = default;
+};
+
+std::ostream &operator<<(std::ostream &os, const I_Printable &obj) {
+    obj.print(os);
+    return os;
+}
+
+class Account : public I_Printable {
 public:
     virtual void withdraw(double amount) {
         std::cout << "In Account::withdraw" << std::endl;
     }
+    virtual void print(std::ostream &os) const override {
+        os << "Account display";
+    }
     virtual ~Account() {}
 };
-
-std::ostream &operator<<(std::ostream &os, const Account &acc) {
-    os << "Account display";
-    return os;
-}
 
 class Checking : public Account {
 public:
     virtual void withdraw(double amount) override {
         std::cout << "In Checking::withdraw" << std::endl;
     }
+    virtual void print(std::ostream &os) const override {
+        os << "Checking display";
+    }
     virtual ~Checking() {}
 };
-
-std::ostream &operator<<(std::ostream &os, const Checking &acc) {
-    os << "Checking display";
-    return os;
-}
 
 class Savings : public Account {
 public:
     virtual void withdraw(double amount) override {
         std::cout << "In Savings::withdraw" << std::endl;
     }
+    virtual void print(std::ostream &os) const override {
+        os << "Savings display";
+    }
     virtual ~Savings() {}
 };
-
-std::ostream &operator<<(std::ostream &os, const Savings &acc) {
-    os << "Savings display";
-    return os;
-}
 
 class Trust : public Account {
 public:
     virtual void withdraw(double amount) override {
         std::cout << "In Trust::withdraw" << std::endl;
     }
+    virtual void print(std::ostream &os) const override {
+        os << "Trust display";
+    }
     virtual ~Trust() {}
 };
 
-std::ostream &operator<<(std::ostream &os, const Trust &acc) {
-    os << "Trust display";
-    return os;
-}
+class Dog : public I_Printable {
+public:
+    virtual void print(std::ostream &os) const override {
+        os << "woof woof";
+    }
+};
 
 int main() {
+    Dog *dog = new Dog();
+    std::cout << *dog << std::endl;
+    delete dog;  // Clean up allocated memory
+
     Account a;
     std::cout << a << std::endl;
 
