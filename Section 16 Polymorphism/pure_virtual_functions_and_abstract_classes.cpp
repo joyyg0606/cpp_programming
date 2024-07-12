9d9da5aa903ad26278f460101ee0c48cf998c655
vxntcy <joyyg0606@gmail.com>
2024-07-12 14:51:49 -0400

    fixed mistakes

--- Section 16 Polymorphism/pure_virtual_functions_and_abstract_classes.cpp
+++ Section 16 Polymorphism/pure_virtual_functions_and_abstract_classes.cpp
@@ -2,22 +2,20 @@
 #include <vector>
 
 class Shape {
-private:
-
 public:
     virtual void draw() = 0;
     virtual void rotate() = 0;
     virtual ~Shape() {}
 };
 
-class Open_Shape: public Shape {
+class Open_Shape : public Shape {
 public:
-    virtual ~Open_shape() {}
+    virtual ~Open_Shape() {}
 };
 
 class Closed_Shape : public Shape {
-public: 
-    virtual ~Closed_Shape() {};
+public:
+    virtual ~Closed_Shape() {}
 };
 
 class Line : public Open_Shape {
@@ -28,10 +26,21 @@ public:
     virtual void rotate() override {
         std::cout << "Rotating a line" << std::endl;
     }
+    virtual ~Line() {}
+};
+
+class Circle : public Closed_Shape {
+public:
+    virtual void draw() override {
+        std::cout << "Drawing a circle" << std::endl;
+    }
+    virtual void rotate() override {
+        std::cout << "Rotating a circle" << std::endl;
+    }
     virtual ~Circle() {}
 };
 
-class Square: public Closed_Shape {
+class Square : public Closed_Shape {
 public:
     virtual void draw() override {
         std::cout << "Drawing a square" << std::endl;
@@ -42,10 +51,30 @@ public:
     virtual ~Square() {}
 };
 
-int main() {
-    Shape s;
-    Shape *p = new Shape();
+void screen_refresh(const std::vector<Shape *> &shapes) {
+    std::cout << "refreshing" << std::endl;
+    for(const auto p: shapes)
+        p->draw();
+}
 
-    
+int main() {
+    Shape *ptr = new Circle();
+    ptr->draw();
+    ptr->rotate();
+
+    Shape *s1 = new Circle();
+    Shape *s2 = new Line();
+    Shape *s3 = new Square();
+
+    std::vector<Shape *> shapes {s1, s2, s3};
+    for (const auto p: shapes) 
+        p->draw();
+
+    // Clean up memory
+    delete ptr;
+    delete s1;
+    delete s2;
+    delete s3;
+
     return 0;
 }
