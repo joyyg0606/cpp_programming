0bd23d377bfb092107d57a5837ae59e758c31894
vxntcy <joyyg0606@gmail.com>
2024-07-12 21:09:49 -0400

    trust_account challenge

---
+++ Section 16 Polymorphism/challenge/Trust_Account.h
@@ -0,0 +1,26 @@
+#ifndef _TRUST_ACCOUNT_H_
+#define _TRUST_ACCOUNT_H_
+
+#include "Savings_Account.h"
+
+class Trust_Account : public Savings_Account {
+    friend std::ostream &operator<<(std::ostream &os, const Trust_Account &account);
+private:
+    static constexpr const char *def_name = "unnamed trust account";
+    static constexpr double def_balance = 0.0;
+    static constexpr double def_int_rate = 0.0;
+    static constexpr double bonuc_amount = 50.0;
+    static constexpr double bonus_threshold = 500.0;
+    static constexpr int max_withdrwals = 3;
+    static constexpr double max_withdraw_percent = 0.2;
+protected:
+    int num_withdrwals;
+public:
+    Trust_Account(std::string name = def_name, double balance = def_balance, double int_rate = def_int_rate);
+
+    virtual bool deposit(double amount) override;
+
+    virtual bool withdraw(double amount) override;
+};
+
+#endif
