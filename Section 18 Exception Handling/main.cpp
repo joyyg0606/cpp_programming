a839ee9c8e5f381d8ec7c611d1e8ef7dece54746
vxntcy <joyyg0606@gmail.com>
2024-07-18 16:46:53 -0400

    added an if for error message

--- Section 18 Exception Handling/main.cpp
+++ Section 18 Exception Handling/main.cpp
@@ -13,8 +13,13 @@ int main() {
     std::cin >> gallons;
 
     // miles_per_gallon = miles/gallons;
-    miles_per_gallon = static_cast<double>(miles) / gallons;
-    std::cout << "Result: " << miles_per_gallon << std::endl;
+    if (gallons != 0) {
+        miles_per_gallon = static_cast<double>(miles) / gallons;
+        std::cout << "Result: " << miles_per_gallon << std::endl;
+    } else {
+        std::cerr << "Sorry, can't divide by zero" << std::endl;
+    }
+    
 
     std::cout << "Bye" << std::endl;
     return 0;
