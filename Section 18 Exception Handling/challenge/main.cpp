57800e32f3e5b9c4e2d106d3db160bbbd8e3c023
vxntcy <joyyg0606@gmail.com>
2024-07-18 20:02:00 -0400

    main for challenge project

---
+++ Section 18 Exception Handling/challenge/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+#include "Account.h"
+#include "Checking_Account.h"
+#include "Savings_Account.h"
+#include "Trust_Account.h"
+#include "Account_util.h"
+
+using namespace std;
+
+int main() {
+    
+    std::cout << "Program completed successfully" << std::endl;
+    return 0;
+}
