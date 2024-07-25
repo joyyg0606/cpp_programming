c4165cf2566f0882c43623bd5eb73f3894844589
vxntcy <joyyg0606@gmail.com>
2024-07-25 15:32:14 -0400

    challenge 4 start

--- Section 19 IO and Streams/challenge_4.cpp
+++ Section 19 IO and Streams/challenge_4.cpp
@@ -4,5 +4,21 @@
 #include <string>
 
 int main() {
-    std::ifstream in_file {}
+    std::ifstream in_file {"../romeoandjuliet.txt"};
+    std::ofstream out_file {"../romeoandjuliet_out.txt"};
+    if (!in_file.is_open()) {
+        std::cerr << "Error opening input file" << std::endl;
+        return 1;
+    }
+    if (!in_file.is_open()) {
+        std::cerr << "Error opening input file" << std::endl;
+        return 1;
+    }
+
+    std::string line{};
+    int line_number {0};
+    while (getline(in_file, line)) {
+        ++line_number;
+        out_file << std::set(7) << std::left << line_number << line << std::endl;
+    }
 }
