f97fac4e84ae8c3262f150fc137905b71b0688d0
vxntcy <joyyg0606@gmail.com>
2024-07-25 18:17:24 -0400

    using string streams.cpp

--- Section 19 IO and Streams/using_string_streams.cpp
+++ Section 19 IO and Streams/using_string_streams.cpp
@@ -13,4 +13,35 @@ int main() {
 
     iss >> name >> num >> total;
     std::cout << std::setw(10) << std::left << name << std::setw(5) << num << std::setw(10) << total << std::endl;
+
+    std::cout << "\n=======" << std::endl;
+
+    std::ostringstream oss {};
+    oss << std::setw(10) << std::left << name << std::set(5) << num << std::setw(10) << total << std::endl;
+    std::cout << oss.str() << std::endl;
+
+    std::cout << "\n Data validation" << std::endl;
+
+    int value{};
+    std::string entry {};
+    bool done = false;
+    do {
+        std::cout << "please enter an integer: ";
+        std::cin >> entry;
+        std::istringstream validator {entry};
+        if(validator >> value)
+            done = true;
+        else
+            std::cout << "Sorry, that's not an integer" << std::endl;
+
+        //discards the input buffer
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    } while (!done);
+
+    std::cout << "you entered the integer: " << value << std::endl;
+
+    std::cout << std::endl;
+
+    return 0;
+
 }
