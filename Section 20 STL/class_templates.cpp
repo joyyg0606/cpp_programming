12b34c4b9691fecbdf9bad26ee6659fcb34873f7
vxntcy <joyyg0606@gmail.com>
2024-07-26 17:03:15 -0400

    class templates

--- Section 20 STL/class_templates.cpp
+++ Section 20 STL/class_templates.cpp
@@ -3,15 +3,14 @@
 #include <vector>
 
 template <typename T>
-class item {
+class Item {
 private:
     std::string name;
     T value;
 public:
-    Item(std::string name, T value) : name{name}, value{value}
-    {}
-    std::string get_name() const {return name;}
-    T get_value() const {return value;}
+    Item(std::string name, T value) : name{name}, value{value} {}
+    std::string get_name() const { return name; }
+    T get_value() const { return value; }
 };
 
 template <typename T1, typename T2>
@@ -21,32 +20,31 @@ struct My_Pair {
 };
 
 int main() {
-
-    Item<int> item 1 {"Frank", 100};
+    Item<int> item1{"Frank", 100};
     std::cout << item1.get_name() << " " << item1.get_value() << std::endl;
 
-    Item <std::string> item2 {"Frank", "Professor"};
+    Item<std::string> item2{"Frank", "Professor"};
     std::cout << item2.get_name() << " " << item2.get_value() << std::endl;
 
-    Item <Item<std::string>> item3 {"Frank", {"C++", "Professor"}};
+    Item<Item<std::string>> item3{"Frank", Item<std::string>{"C++", "Professor"}};
     std::cout << item3.get_name() << " " << item3.get_value().get_name() << " " << item3.get_value().get_value() << std::endl;
 
     std::cout << "\n================================" << std::endl;
-    std::vector<item<double>> vec{};
-    vec.push_back(item<double>("Larry", 100.0));
+    std::vector<Item<double>> vec{};
+    vec.push_back(Item<double>("Larry", 100.0));
     vec.push_back(Item<double>("Moe", 200.0));
-    vec.push_back(Item<double>("Curly",300.0));
+    vec.push_back(Item<double>("Curly", 300.0));
 
-    for (const auto &item:vec) {
+    for (const auto &item : vec) {
         std::cout << item.get_name() << " " << item.get_value() << std::endl;
     }
 
     std::cout << "\n==========" << std::endl;
-    My_Pair <std::string, int> p1 {"Frank", 100};
-    My_Pair <int, double> p2 {124, 13.6};
+    My_Pair<std::string, int> p1{"Frank", 100};
+    My_Pair<int, double> p2{124, 13.6};
 
     std::cout << p1.first << ", " << p1.second << std::endl;
-    std::cout << p2.first << ", " << p2.second << std::endl;
+    std::cout << p2.first << ", p2.second << std::endl;
 
     return 0;
 }
