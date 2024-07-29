87feba7f6f10b4efdaea329ab0fa6869ef2f4bfe
vxntcy <joyyg0606@gmail.com>
2024-07-29 15:02:33 -0400

    introductionto algorithms finish

--- Section 20 STL/introduction_to_algorithms.cpp
+++ Section 20 STL/introduction_to_algorithms.cpp
@@ -9,9 +9,12 @@ class Person {
     int age;
 public:
     Person() = default;
-    Person(std::string name, int age) : name{name}, age{age} {} bool operator<(const Person &rhs) const {
+    Person(std::string name, int age) : name{name}, age{age} {}
+
+    bool operator<(const Person &rhs) const {
         return this->age < rhs.age;
     }
+
     bool operator==(const Person &rhs) const {
         return (this->name == rhs.name && this->age == rhs.age);
     }
@@ -20,7 +23,8 @@ public:
 void find_test() {
     std::cout << "\n=================================" << std::endl;
 
-    std::vector<int> vec{1,2,3,4,5};
+    // Finding an element in a vector
+    std::vector<int> vec{1, 2, 3, 4, 5};
     auto loc = std::find(std::begin(vec), std::end(vec), 1);
 
     if (loc != std::end(vec))
@@ -28,6 +32,7 @@ void find_test() {
     else
         std::cout << "Couldn't find the number" << std::endl;
 
+    // Finding an element in a list of Person objects
     std::list<Person> players {
         {"Larry", 18},
         {"Moe", 20},
@@ -35,7 +40,7 @@ void find_test() {
     };
 
     auto loc1 = std::find(players.begin(), players.end(), Person{"Moe", 20});
-    if(loc1 != players.end())
+    if (loc1 != players.end())
         std::cout << "Found Moe" << std::endl;
     else   
         std::cout << "Moe not found" << std::endl;
@@ -44,20 +49,29 @@ void find_test() {
 void count_test() {
     std::cout << "\n=================================" << std::endl;
 
-    std::vector<int> vec {1,2,3,4,5,1,2,1};
+    // Counting occurrences of an element in a vector
+    std::vector<int> vec {1, 2, 3, 4, 5, 1, 2, 1};
 
     int num = std::count(vec.begin(), vec.end(), 1);
-    std::cout << num << " occurences found" << std::endl;
+    std::cout << num << " occurrences found" << std::endl;
 }
 
 void count_if_test() {
     std::cout << "\n======================================" << std::endl;
 
-    std::vector<int> vec {1,2,3,4,5,1,2,1,100};
-    int num = std::count_if(vec.begin(), vec.end(), [](inx) {return x %2 != 0;});
+    // Counting elements that meet a specific condition
+    std::vector<int> vec {1, 2, 3, 4, 5, 1, 2, 1, 100};
+
+    int num = std::count_if(vec.begin(), vec.end(), [](int x) { return x % 2 == 0; });
     std::cout << num << " even numbers found" << std::endl;
 
-    num = std::count_if(vec.begin(), vec.end(), [](int x) {return x %2 != 0;});
+    num = std::count_if(vec.begin(), vec.end(), [](int x) { return x % 2 != 0; });
     std::cout << num << " odd numbers found" << std::endl;
+}
 
+int main() {
+    find_test();
+    count_test();
+    count_if_test();
+    return 0;
 }
