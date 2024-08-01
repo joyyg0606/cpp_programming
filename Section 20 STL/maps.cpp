bcca6aa75825db1ef7b2f4c74a4e2d68a72e6b65
vxntcy <joyyg0606@gmail.com>
2024-08-01 13:56:17 -0400

    maps test 1 and 2

--- Section 20 STL/maps.cpp
+++ Section 20 STL/maps.cpp
@@ -21,3 +21,62 @@ void display(const std::map<T1, T2> &l) {
     }
     std::cout << "] " << std::endl;
 }
+
+void test1() {
+    std::cout << "test 1" << std::endl;
+    std::map<std::string, int> m {
+        {"Larry", 3},
+        {"moe", 1},
+        {"curly", 2}
+    };
+    display(m);
+
+    m.insert(std::pair<std::string, int>("Anna", 10));
+    display(m);
+
+    m.insert(std::make_pair("Joe", 5));
+    display(m);
+
+    m["Frank"] = 18;
+    display(m);
+
+    m["Frank"] += 10;
+    display(m);
+
+    std::cout << "count for joe: " << m.count("Joe") << std::endl;
+    std::cout << "count for frank: " << m.count("Frank") << std::endl;
+
+    auto it = m.find("Larry");
+    if(it != m.end())
+        std::cout << "found: " << it->first << ":" << it->second << std::endl;
+    
+    m.clear();
+    display(m);
+}
+
+void test2() {
+    std::cout << "test 2" << std::endl;
+
+    std::map<std::string, std::set<int>> grades {
+        {"Larry", {100,90}},
+        {"moe", {94}},
+        {"curly", {80,90,100}}
+    };
+    
+    display(grades);
+
+    grades["Larry"].insert(95);
+
+    display(grades);
+
+    auto it = grades.find("Moe");
+    if(it != grades.end())
+    {
+        it->second.insert(1000);
+    }
+    display(grades);
+}
+
+int main() {
+    
+}
