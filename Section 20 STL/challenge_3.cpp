d9ff121cd162a00bb8d376639bcb1c7fad32dfd8
vxntcy <joyyg0606@gmail.com>
2024-08-01 15:31:56 -0400

    challenge start

---
+++ Section 20 STL/challenge_3.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <map>
+#include <set>
+
+void display_words(const std::map<std::string, int> &words) {
+    std::cout << std::setw(12) << std::left << "\nWord" << std::setw(7) << std::right << "Count" << std::endl;
+    std::cout << "======================" << std::endl;
+    for (auto pair : words)
+        std::cout << std::setw(12) << std::left << pair.first << std::setw(7) << std::right << pair.second << std::endl;
+}
+
+void display_words(const std::map<std::string, std::set<int>> &words) {
+    std::cout << std::setw(12) << std::left << "\nWord" << "Occurrences" << std::endl;
+    std::cout << "================================================" << std::endl;
+    for (auto pair : words) {
+        std::cout << std::setw(12) << std::left << pair.first << std::left << "[ ";
+        for (auto i : pair.second)
+            std::cout << i << " ";
+        std::cout << "]" << std::endl;
+    }
+}
+
+std::string clean_string(const std::string &s) {
+    std::string result;
+    for (char c : s) {
+        if (c == '.' || c == ',' || c == ';' || c == ':')
+            continue;
+        else
+            result += c;
+    }
+    return result;
+}
+
+void part1() {
+    std::map<std::string, int> words;
+    std::string line;
+    std::string word;
+    std::ifstream in_file{"../words.txt"};
+    if (in_file) {
+
+        in_file.close();
+        display_words(words);
+    } else {
+        std::cerr << "Error opening input file" << std::endl;
+    }
+}
+
+void part2() {
+    std::map<std::string, std::set<int>> words;
+    std::string line;
+    std::string word;
+    std::ifstream in_file{"../words.txt"};
+    if (in_file) {
+
+        in_file.close();
+        display_words(words);
+    } else {
+        std::cerr << "Error opening input file" << std::endl;
+    }
+}
+
+int main() {
+    part1();
+    part2();
+    return 0;
+}
