55b2471cbba5bed47129659c19e18c41276e16c0
vxntcy <joyyg0606@gmail.com>
2024-08-02 16:26:41 -0400

    added queue include

--- Section 20 STL/queue.cpp
+++ Section 20 STL/queue.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 template <typename T>
 void display(std::queue<T> q) {
@@ -14,33 +15,41 @@ void display(std::queue<T> q) {
 int main() {
     std::queue<int> q;
 
-    for (int i: {1,2,3,4,5})
+    // Push initial elements into the queue
+    for (int i : {1, 2, 3, 4, 5})
         q.push(i);
+    
+    // Display the queue
     display(q);
     std::cout << "Front: " << q.front() << std::endl;
     std::cout << "Back: " << q.back() << std::endl;
 
+    // Push a new element and display the queue
     q.push(100);
     display(q);
 
+    // Pop two elements and display the queue
     q.pop();
     q.pop();
     display(q);
 
+    // Empty the queue and display it
     while (!q.empty())
         q.pop();
     display(q);
     
     std::cout << "Size: " << q.size() << std::endl;
 
+    // Push new elements and display the queue
     q.push(10);
     q.push(100);
     q.push(1000);
     display(q);
 
     std::cout << "Front: " << q.front() << std::endl;
-    std::cout << "back: " << q.back() << std::endl;
+    std::cout << "Back: " << q.back() << std::endl;
 
+    // Modify the front and back elements and display the queue
     q.front() = 5;
     q.back() = 5000;
 
